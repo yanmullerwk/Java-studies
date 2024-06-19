00a1b34a33277b674d7f9a5315bf6a2b0507fb0d
yan muller <yan.muller.contact@gmail.com>
2024-06-18 21:39:15 -0300

    update studies

--- loop2.java
+++ loop2.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+//imprimir a sequencia 1 2 3 4 5 10 20 30 40 50 em um array;
+public class loop2 {
 
-public class loops1 {
     public static void main(String[] args) {
         ArrayList<Integer> num = new ArrayList<>();
 
-        for(int i = 10; i >= 1; i--){
+        for(int i = 1; i<=5; i++){
             num.add(i);
         }
+
+        for(int i = 10; i<=50;i+=10){
+            num.add(i);
+        }
+        
         System.out.println(num);
     }
 }
