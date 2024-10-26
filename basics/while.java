b14793436814446bc087e9effd909dd8b47fa616
yanmullerwk <yan.muller.contact@gmail.com>
2024-10-25 21:36:15 -0300

    while add

---
+++ basics/while.java
@@ -0,0 +1,9 @@
+public class While {
+   public static void main(String[] args) {
+      int cont = 0;
+      while (cont!=11) {
+         System.out.println(cont);
+         cont++;
+      }
+   }
+}
